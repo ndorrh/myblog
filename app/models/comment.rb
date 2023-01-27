692c74002306a1b13868710130411cdb6988cb4e
ndorrh <ndorrh@gmail.com>
2023-01-27 01:41:28 +0100

    Add name validation to User model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+  validates :name, presence: true
 
   after_save :update_comments_counter_for_post
 
