67eee187a368ce804a418ad747ad9a394b65a49a
ndorrh <ndorrh@gmail.com>
2023-01-27 05:42:30 +0100

    Add test for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{Post.new(title: 'My first post', author_id: 1, text:'my post is greate', comments_counter: 0, likes_counter: 0)}
+
+  before {subject.save}
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'Comments Counter should be greater than or equal to 0' do
+    subject.comments_counter = -7
+    expect(subject).to_not be_valid
+  end
+
+  it 'Comments Counter should be greater than or equal to 0' do
+    subject.author_id = 1
+    subject.comments_counter = '60'
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes Counter should be greater than or equal to 0' do
+    subject.likes_counter = -12
+    expect(subject).to_not be_valid
+  end
+
+  it 'likes Counter should be greater than or equal to 0' do
+    subject.author_id = 1
+    subject.likes_counter = '13'
+    expect(subject).to_not be_valid
+  end
+
+  it 'author_id should be greater than or equal to 0' do
+    
+    expect(subject.author_id).to be_integer
+  end
+
+  it 'title should not be too long' do
+    subject.title = 'a' * 251
+    expect(subject).to_not be_valid
+  end
+
+  it 'Return last five posts for user' do
+    expect(subject.five_most_recent_comments).to eq([])
+  end
+  
 end
