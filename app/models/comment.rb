7e3cd3eb48507806b1d3ffd33a28c69182ed58fd
ndorrh <ndorrh@gmail.com>
2023-01-27 03:17:12 +0100

    Add posts_counter validation to User model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
   validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, allow_nil: true, greater_than_or_equal_to: 0 }
 
   after_save :update_comments_counter_for_post
 
