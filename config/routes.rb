6e7cefe186a2b97d2fb7c3074ab3941280dc5489
ndorrh <ndorrh@gmail.com>
2023-01-30 16:10:11 +0100

    Add routes and Create Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  get 'users/:id', to: 'users#show'
+  get '/users', to: 'users#index', as: :users
+  get 'users/:id', to: 'users#show', as: :user
+  get 'users/:id/posts', to: 'posts#index', as: :user_posts
+  get 'users/:id/posts/:id', to: 'posts#show', as: :user_post
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
