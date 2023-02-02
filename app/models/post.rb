72b1e312b83a283022a75f8cf6c5b2e8be4559ae
ndorrh <ndorrh@gmail.com>
2023-02-02 19:30:59 +0100

    Set default value for comments_counter, posts_counter and likes_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,15 @@
 class Post < ApplicationRecord
+  attribute :likes_counter, :integer, default: 0
+  attribute :comments_counter, :integer, default: 0
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_posts_counter_for_user
 
