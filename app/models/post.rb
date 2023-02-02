2133e3a1131756c1538f6567b4d35aa03faa5d8d
ndorrh <ndorrh@gmail.com>
2023-02-02 18:04:39 +0100

    implemented requested changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
 
   after_save :update_posts_counter_for_user
 
