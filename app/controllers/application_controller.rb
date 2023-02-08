5837f9a7ac6911f6cbe6c628dc3c978505b35ec5
Tiago Lelinski Marin <tiago.lelinski@gmail.com>
2023-02-08 13:24:13 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   private
 
@@ -11,6 +10,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name bio photo password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name password])
   end
 end
